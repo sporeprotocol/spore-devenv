00804712dc08ef373e6940aaf4de7542f5c6491f
Dawn-githup <13477199767@163.com>
2024-01-08 23:08:04 +0800

    1.add notes

--- prepare.sh
+++ prepare.sh
@@ -1,9 +1,12 @@
+# Pull project lumos/spore-contract
 git clone https://github.com/ckb-js/lumos.git
 git clone https://github.com/Dawn-githup/spore-contract.git
 
+# Build spore-contract
 cd spore-contract
 cargo install cross --git https://github.com/cross-rs/cross
 cargo install ckb-capsule --git https://github.com/nervosnetwork/capsule.git --tag v0.10.2
 capsule build --release
 
+npm install
 npm run build:lumos
