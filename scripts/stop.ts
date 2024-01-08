9e1db6deb01dae0d9717819f586a83c7ddf5a0a9
Dawn-githup <13477199767@163.com>
2024-01-09 02:20:05 +0800

    Terminate ckb process

--- scripts/stop.ts
+++ scripts/stop.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import { exec } from 'child_process';
 
 // Function to delete a directory and its contents
 const deleteDirectory = (directoryPath: string): void => {
@@ -19,26 +20,65 @@ const deleteDirectory = (directoryPath: string): void => {
     }
 };
 
-// Function to rename a file
-const renameFile = (oldFileName: string, newFileName: string): void => {
-    const oldFilePath = path.join(__dirname, oldFileName);
-    const newFilePath = path.join(__dirname, newFileName);
+// Function to delete a file
+const deleteFile = (fileName: string): void => {
+    const filePath = path.join(process.cwd(), fileName);
 
-    if (fs.existsSync(oldFilePath)) {
-        fs.renameSync(oldFilePath, newFilePath);
-        console.log(`Renamed ${oldFileName} to ${newFileName}.`);
-    } else {
-        console.log(`${oldFileName} not found.`);
+    try {
+        if (fs.existsSync(filePath)) {
+            fs.unlinkSync(filePath);
+            console.log(`Deleted ${fileName}.`);
+        } else {
+            console.log(`${fileName} not found.`);
+        }
+    } catch (error: any) {
+        console.error(`Error deleting ${fileName}: ${error.message}`);
     }
 };
+
+
+
+function killCKBNode() {
+    const ckbPort = 8114;
+    let killCommand = '';
+
+    switch (process.platform) {
+        case 'win32':
+            killCommand = `taskkill /F /IM ckb.exe`;
+            break;
+        case 'darwin':
+        case 'linux':
+            // Use the kill command to terminate the process
+            killCommand = `kill -9 $(lsof -ti:${ckbPort})`;
+            break;
+        default:
+            console.error(`Unsupported operating system: ${process.platform}`);
+            return;
+    }
+
+    exec(killCommand, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error while killing CKB node: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.error(`CKB node termination error: ${stderr}`);
+            return;
+        }
+        console.log('CKB node terminated successfully.');
+    });
+}
 
 // Main function
 const main = (): void => {
     // Delete the 'tmp' directory
     deleteDirectory('tmp');
 
-    // Rename 'lumos.json' to 'config.json'
-    renameFile('lumos.json', 'config.json');
+    // Delete the 'config.json' file
+    deleteFile('config.json');
+
+    // Terminate CKB node
+    killCKBNode()
 
     console.log('Commands executed successfully.');
 };
