263bdff72f89021edff888ccae6fdf3e61cbdb97
Dawn-githup <13477199767@163.com>
2024-01-09 00:26:47 +0800

    Fix local .env not being read.

--- tests/data.preparation.test.ts
+++ tests/data.preparation.test.ts
@@ -1,10 +1,23 @@
-import {e2eProvider} from "../src/config";
-import {BI} from "@ckb-lumos/bi";
-import { randomSecp256k1Account} from "../src/utils";
+import { config } from 'dotenv';
+import { resolve } from 'path';
+import { e2eProvider } from '../src/config';
+import { BI } from '@ckb-lumos/bi';
+import { randomSecp256k1Account } from '../src/utils';
 
-const accountCharlie = process.env.VITE_ACCOUNT_CHARLIE;
-const accountAlice = process.env.VITE_ACCOUNT_ALICE;
+let accountCharlie = process.env.VITE_ACCOUNT_CHARLIE;
+let accountAlice = process.env.VITE_ACCOUNT_ALICE;
 
+// Check if environment variables are empty
+if (!accountCharlie || !accountAlice) {
+    // Load environment variables from .env file
+    config({ path: resolve(__dirname, '../.env') });
+
+    // Reassign the values after loading from .env
+    accountCharlie = process.env.VITE_ACCOUNT_CHARLIE;
+    accountAlice = process.env.VITE_ACCOUNT_ALICE;
+}
+
+// If after loading .env file, environment variables are still empty, throw an error
 if (!accountCharlie || !accountAlice) {
     throw new Error('Missing account information. Please check environment variables.');
 }
@@ -13,8 +26,8 @@ const CHARLIE = randomSecp256k1Account(accountCharlie);
 const ALICE = randomSecp256k1Account(accountAlice);
 
 describe('CKBytes', function () {
-    it("should have claimed CKBytes for charlie/alice", async () => {
+    it('should have claimed CKBytes for charlie/alice', async () => {
         await e2eProvider.claimCKB({ claimer: CHARLIE.address, amount: BI.from(1000000 * 10 ** 8) });
         await e2eProvider.claimCKB({ claimer: ALICE.address, amount: BI.from(1000000 * 10 ** 8) });
-    })
+    });
 });
