74f5d4b40d787c943e858109b7dd3fcbfb5aec65
Dawn-githup <13477199767@163.com>
2024-01-10 05:54:39 +0800

    Use custom commit hash of spore-contract as parameter for prepare.sh

--- README.md
+++ README.md
@@ -1,8 +1,32 @@
 # spore_devnet
 
-``` sh
-sh prepare.sh
-```
+## Prepare Script
+
+This script automates the process of cloning a Git repository, checking out a specific branch or commit hash, and can be customized with various options.
+
+### Usage Examples
+
+You can use command-line options to override default settings:
+
+- **Specify Branch:**
+  ```bash
+  ./prepare.sh -b master
+
+- **Specify Commit Hash:**
+  ```bash
+  ./prepare.sh -c abcdef123456
+
+- **Specify Repository URL:**
+  ```bash
+  ./prepare.sh -r https://github.com/your-username/your-repo.git
+
+- **Combined Options:**
+    - **Specify Branch and Commit Hash:**
+      ```bash
+      ./prepare.sh -b develop -c abcdef123456
+    - **Change Repository URL and Specify Commit Hash:**
+      ```bash
+      ./prepare.sh -r https://github.com/your-username/your-repo.git -c abcdef123456
 
 Generate spore dev data
 ```shell
--- prepare.sh
+++ prepare.sh
@@ -1,9 +1,56 @@
-# Pull project lumos/spore-contract
+# Pull project lumos
 git clone https://github.com/ckb-js/lumos.git
-git clone https://github.com/sporeprotocol/spore-contract.git
+# Default branch is master
+default_branch="master"
+# Default repository URL
+repo_url="https://github.com/sporeprotocol/spore-contract.git"
+
+# Parse command-line arguments
+while [[ $# -gt 0 ]]; do
+    key="$1"
+
+    case $key in
+        -c|--commit-hash)
+        # Specify commit hash
+        commit_hash="$2"
+        shift
+        shift
+        ;;
+        -b|--branch)
+        # Specify branch (overrides default)
+        default_branch="$2"
+        shift
+        shift
+        ;;
+        -r|--repo-url)
+        # Specify repository URL
+        repo_url="$2"
+        shift
+        shift
+        ;;
+        *)
+        # Unknown option
+        echo "Unknown option: $1"
+        exit 1
+        ;;
+    esac
+done
+
+# Use the provided commit hash if available
+if [ -n "$commit_hash" ]; then
+    default_branch="$commit_hash"
+fi
+
+# Clone the repository
+git clone $repo_url
+
+# Enter the repository directory
+cd spore-contract
+
+# Checkout the specified branch or commit hash
+git checkout $default_branch
 
 # Build spore-contract
-cd spore-contract
 cargo install cross --git https://github.com/cross-rs/cross
 cargo install ckb-capsule --git https://github.com/nervosnetwork/capsule.git --tag v0.10.2
 capsule build --release
