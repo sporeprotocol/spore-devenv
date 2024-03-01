86953f653897b8a98763efe374776ead82e8ce42
Dawn-githup <77844990+Dawn-githup@users.noreply.github.com>
2024-03-01 15:31:12 +0800

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -58,7 +58,8 @@ git checkout $default_branch
 
 # Build spore-contract
 cargo install cross --git https://github.com/cross-rs/cross
-cargo install ckb-capsule --git https://github.com/nervosnetwork/capsule.git --tag v0.10.1
+curl -L https://github.com/nervosnetwork/capsule/releases/download/v0.10.2/capsule_v0.10.2_x86_64-linux.tar.gz -o /tmp/capsule.tar.gz
+tar -zxf /tmp/capsule.tar.gz -C /tmp
 capsule build --release
 
 npm install
